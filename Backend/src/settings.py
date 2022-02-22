e979a0893eef0efe935852ea7a1be5c7b4bc140c
KevinCamos <kevincamossoto@gmail.com>
2022-02-22 16:10:22 +0100

    Prueba 22022022

--- Backend/src/settings.py
+++ Backend/src/settings.py
@@ -108,7 +108,11 @@ WSGI_APPLICATION = 'src.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # try to load local_settings.py if it exists
 try:
