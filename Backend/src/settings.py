8b24a7d1e5ae142789e7778ef80bd8119da5fe6a
KevinCamos <kevincamossoto@gmail.com>
2022-02-22 16:07:03 +0100

    Prueba 22022022

--- Backend/src/settings.py
+++ Backend/src/settings.py
@@ -102,12 +102,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://dev.to/sm0ke/how-to-use-mysql-with-django-for-beginners-2ni0
 
 # we only need the engine name, as heroku takes care of the rest
-DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # try to load local_settings.py if it exists
