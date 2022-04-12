bf9b8d750269246d1e1820e1b9014134653da9f5
KevinCamos <kevincamossoto@gmail.com>
2022-04-12 19:58:33 +0200

    Backend Rest: Connect in Django server

--- Backend/Rest/routes/api/simulatorBank.js
+++ Backend/Rest/routes/api/simulatorBank.js
@@ -1,43 +1,19 @@
-var router = require("express").Router();
+const router = require("express").Router();
 
-// var auth = require("../auth");
-
-// Preload user profile on routes with ':username'
-router.param("username", function (req, res, next, username) {
-  // User.findOne({ username: username })
-  //   .populate("following")
-  //   .populate("followers")
-  //   .then(function (user) {
-  //     if (!user) {
-  //       return res.sendStatus(404);
-  //     }
-
-  //     Order.aggregate([
-  //       { $project: { _id: 1, id_user_seller: 1, valoration: 1 } },
-  //       { $match: { id_user_seller: user._id } },
-  //       { $group: { _id: "id_user_seller", media: { $avg: "$valoration" } } },
-  //     ]).then(function (valoration) {
-  //       console.log(valoration);
-  //       req.profile = user;
-  //       if(valoration[0]){
-  //         req.valoration = valoration[0].media?valoration[0].media:0 ;
-
-  //       }else{
-  //         req.valoration =0
-  //       }
-  //       return next();
-  //     });
-  //     // console.log("entra param");
-  //   })
-  //   .catch(next);
-});
+const request = require("../../middleware/requests");
 
 
 
 /* PROFILE */
-router.get("/:username", /* auth.optional, */ function (req, res, next) {
+router.post("/", /* auth.optional, */  async  (req, res, next)=> {
+
+  console.log("eh")
+  user = await request.get_user_token("eh");
+  console.log(user)
+
+  return res.json({ user });
   // if (req.payload) {
-  //   /* SI ESTÁS REGISTRAT HAS DE VEURE ELS SEUS FOLLOWINGS, ELS FOLLOWERS I SI LI FAS FOLLOW */
+  //   /* SI ESTÁS REGISTRAT HAS DE VEURE EasdLS SEUS FOLLOWINGS, ELS FOLLOWERS I SI LI FAS FOLLOW */
   //   User.findById(req.payload.id).then(function (user) {
   //     if (!user) {
   //       return res.json({
@@ -66,24 +42,6 @@ router.get("/:username", /* auth.optional, */ function (req, res, next) {
   //     ),
   //   });
   // }
-});
-
-/* FOLLOW */
-router.post("/:userfollow/follow", /* auth.required, */ function (req, res, next) {
-  // var profileId = req.profile._id;
-
-  // User.findById(req.payload.id)
-  //   .then(function (user) {
-  //     if (!user) {
-  //       return res.sendStatus(401);
-  //     }
-  //     // console.log(user)
-  //     user.updateKarma(40, req.profile);
-  //     return user.follow(profileId, req.profile).then(function () {
-  //       return res.json({ profile: req.profile.toProfileJSONFor(user) });
-  //     });
-  //   })
-  //   .catch(next);
 });
 
 module.exports = router;
