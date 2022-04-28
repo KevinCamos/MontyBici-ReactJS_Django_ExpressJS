bb9d9737084fb4f4847b0c74a1627034f43932e1
KevinCamos <kevincamossoto@gmail.com>
2022-04-28 15:29:54 +0200

    Merge branch 'cypress' into improvements/test

--- Frontend/src/hooks/useRegisterCredit.jsx
+++ Frontend/src/hooks/useRegisterCredit.jsx
@@ -29,28 +29,29 @@ const useRegisterCredit = () => {
 
   const createTable = (registers) => {
     const makingRows = [];
-    registers.map((register, index, allRegisters) => {
+    registers.reverse().map((register) => {
       return makingRows.push(createData(
-        allRegisters[allRegisters.length - index - 1]?.created_at,
-        allRegisters[allRegisters.length - index - 1]?.movement,
-        allRegisters[allRegisters.length - index - 1]?.amount));
+        register?.created_at,
+        register?.movement,
+        register?.amount));
     });
     setRows(makingRows);
   };
 
 
-  function createData(created_at, movement, amount) {
-
+  const createData = (created_at = "", movement = "", amount = "") => {
+    if (created_at === "") {
+      return
+    }
     let created_at_date = new Date(created_at)
-    created_at = created_at_date.toLocaleString()
-    let eh = "eh"
+    if (created_at_date.toLocaleString() !== "Invalid Date") {
+      if (new Date(created_at).getTime() > Date.now()) {
+        created_at_date = Date.now()
+      }
+      created_at = created_at_date.toLocaleString()
+    }
     return { created_at, movement, amount };
   }
-
-
-
-
-
   return { isLoading, isError, rows };
 };
 export default useRegisterCredit;
