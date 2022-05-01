cc52cce5c8ecf580f4660308fa003f78e7a478fb
KevinCamos <kevincamossoto@gmail.com>
2022-05-01 12:51:03 +0200

    Frontend: Cors

--- Frontend/src/services/UserServices.jsx
+++ Frontend/src/services/UserServices.jsx
@@ -2,17 +2,17 @@ import Api from './Api';
 
 const userServices = {
   login(data) {
-    return Api().post('auth/users/login/', data);
+    return Api().post('auth/users/login', data);
   },
   register(data) {
-    return Api().post('auth/users/', data);
+    return Api().post('auth/users', data);
   },
 
   checkUser() {
-    return Api().get('auth/user/');
+    return Api().get('auth/user');
   },
   checkAdmin() {
-    return Api().get('auth/user-staff/');
+    return Api().get('auth/user-staff');
   }
 };
 
